7a7f36ebf50fe731ae4117ced346939cd0c9bb4c
John Savin <johnsavin@proton.me>
2023-11-21 13:45:32 -0500

    added an environment api endpoint

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 import { MongoClient } from "mongodb"
 import express from "express"
 import cors from "cors"
-import 'dotenv/config'
 
-const PORT = process.env.PORT 
+const PORT = 8080
 
 const app = express()
 app.use(cors())
@@ -14,25 +13,18 @@ client.connect()
 const db = client.db('DinoHunters')
 const dinosaurs = db.collection('dinosaurs')
 
-// Get all dinosaurs on Main Home Page
-app.get('/', async (req, res) => {
-const allDinosaurs = await dinosaurs.find().toArray()
-res.send(allDinosaurs)
-})
-// List all dinosaurs by category ??
-app.get('/dinosaurPage', async (req, res) => {
-  const dinosaurList = await dinosaurs.find().toArray()
+// List all dinosaurs by enviroment
+app.get('/dinosaurPage/{environment}', async (req, res) => {
+  const environment = req.params.environment
+  const dinosaurList = await dinosaurs.find({environment: environment}).toArray()
   res.send(dinosaurList)
-})
+});
+
 // Add a dinosaur to list
 app.post('/addDinosaur', async (req, res) => {
-  const addOneDinosaur = await dinosaurs.insertOne({name: req.body.name, location: req.body.location, type: req.body.type, description: req.body.description})
+  const addOneDinosaur = await dinosaurs.insertOne({name: req.body.name, environment: req.body.environment, diet: req.body.diet, description: req.body.description})
   res.status(201).send(addOneDinosaur)
-})
+});
 
 
-
-
-app.listen(PORT, () = {
-console.log(`Listening to API on ${PORT}`)
-})
+app.listen(PORT, () => console.log(`API listening on port ${PORT}`))
